8248a45dc9f02052d7b6b70e866ed95efdc30fcb
ss1237 <59708775+ss1237@users.noreply.github.com>
2020-12-17 00:13:19 -0500

    Update Vector.java

--- Vector.java
+++ Vector.java
@@ -1,10 +1,11 @@
 public class Vector {
 
-	double x, y, mag;
+	double x, y, mag, angle;
 	public Vector(double x, double y) {
 		this.x = x;
 		this.y = y;
 		mag = Math.sqrt(x * x + y * y);
+		angle = Math.atan2(y, x);
 	}
 	
 	public Vector add(Vector v) {
@@ -23,8 +24,9 @@ public class Vector {
 		return x * v.y - y * v.x;
 	}
 	
-	public double angle(Vector v) {
-		return Math.atan2(cross(v), dot(v));
+	public double angle(Vector v) { //angle from 0 to pi
+		double bet = Math.max(angle, v.angle) - Math.min(angle, v.angle);
+		return Math.min(bet, 2 * Math.PI - bet);
 	}
 	
 }
